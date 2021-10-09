191a2324b25a5e78b08a804ab8bed98200d18f1b
Gustavo <gustavotrar@gmail.com>
2021-10-09 12:37:42 -0300

    Desenvolvimento e componentização do modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
-import Modal from 'react-modal'
+import Modal from 'react-modal';
 import { useState } from 'react';
+
+import { NewTransactionModal } from './components/NewTransactionModal'
 import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
 import { GlobalStyle } from './styles/global'
@@ -21,13 +23,10 @@ export function App() {
     <>
       <Header onOpenNewTransactionModal = {handleOpenNewTransactionModal} />
       <Dashboard />
-
-      <Modal 
-          isOpen={isNewTransactionModalOpen}
-          onRequestClose={handleCloseNewTransactionModal}
-        >
-          <h2>Cadastrar transação</h2>
-      </Modal>
+      <NewTransactionModal 
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
 
       <GlobalStyle />
     </>
